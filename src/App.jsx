37f9b0e7a738db076e3281f8d4d834e7761dc1d4
James Troughton <jamesstroughton@gmail.com>
2021-09-02 18:00:04 +1200

    🎉 first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+import { ChannelContainer, ChannelListContainer } from "./components";
+
+const client = StreamChat.getInstance(process.env.API_KEY);
+
+const App = () => {
+  return (
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
+    </div>
+  );
+};
+
+export default App;
