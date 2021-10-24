652c6e02ced77b97d21323ca96ffd5f2d773ab5d
James Troughton <jamesstroughton@gmail.com>
2021-10-24 13:58:16 +1300

    ✨Edit Channel Functionality

--- src/components/EditChannel.jsx
+++ src/components/EditChannel.jsx
@@ -1,7 +1,156 @@
-import React from "react";
+import React, { useState } from "react";
+import { useChatContext } from "stream-chat-react";
+import styled from "styled-components";
 
-const EditChannel = () => {
-  return <div></div>;
+import { UserList } from "./";
+import { CloseCreateChannel } from "../assets";
+
+const ChannelNameInput = ({ channelName = "", setChannelName }) => {
+  const handleChange = (event) => {
+    event.preventDefault();
+
+    setChannelName(event.target.value);
+  };
+
+  return (
+    <ChannelNameInputWrapper>
+      <p>Name</p>
+      <input
+        value={channelName}
+        onChange={handleChange}
+        placeholder="channel-name"
+      />
+      <p>Add Members</p>
+    </ChannelNameInputWrapper>
+  );
+};
+
+const EditChannel = ({ setIsEditing }) => {
+  const { channel } = useChatContext();
+  const [channelName, setChannelName] = useState(channel?.data?.name);
+  const [selectedUsers, setSelectedUsers] = useState([]);
+
+  const updateChannel = async (event) => {
+    event.preventDefault();
+
+    const nameChanged = channelName != (channel.data.name || channel.data.id);
+
+    if (nameChanged) {
+      await channel.update(
+        { name: channelName },
+        { text: `Channel name changed to ${channelName}` }
+      );
+    }
+    if (selectedUsers.length) {
+      await channel.addMembers(selectedUsers);
+    }
+    setChannelName(null);
+    setIsEditing(false);
+    setSelectedUsers([]);
+  };
+
+  return (
+    <EditChannelContainer>
+      <EditChannelHeader>
+        <p>Edit Channel</p>
+        <CloseCreateChannel setIsEditing={setIsEditing} />
+      </EditChannelHeader>
+      <ChannelNameInput
+        channelName={channelName}
+        setChannelName={setChannelName}
+      />
+      <UserList setSelectedUsers={setSelectedUsers} />
+      <EditChannelButtonWrapper onClick={updateChannel}>
+        <p>Save Changes</p>
+      </EditChannelButtonWrapper>
+    </EditChannelContainer>
+  );
 };
 
 export default EditChannel;
+
+const ChannelNameInputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 169px;
+  padding-left: 20px;
+  box-shadow: 0px 1px 0px rgba(0, 0, 0, 0.1);
+
+  input {
+    font-family: Helvetica Neue, sans-serif;
+    font-size: 18px;
+    color: rgba(0, 0, 0, 0.5);
+    height: 50px;
+    width: 540px;
+    background: #f7f6f8;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    box-sizing: border-box;
+    border-radius: 8px;
+    padding-left: 16px;
+  }
+
+  input:focus {
+    border: 1px solid var(--primary-color);
+    outline: none;
+  }
+
+  input::placeholder {
+    font-weight: 300;
+    color: rgba(0, 0, 0, 0.5);
+  }
+
+  p {
+    font-family: Helvetica Neue, sans-serif;
+    font-size: 16px;
+    line-height: 120%;
+    color: #2c2c30;
+    margin-top: 30px;
+  }
+`;
+
+const EditChannelContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+const EditChannelHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 62px;
+  box-shadow: 0px 1px 0px rgba(0, 0, 0, 0.1);
+  padding-right: 20px;
+
+  p {
+    font-family: Helvetica Neue, sans-serif;
+    font-weight: bold;
+    font-size: 18px;
+    line-height: 22px;
+    color: #2c2c30;
+    margin-left: 20px;
+  }
+
+  svg {
+    cursor: pointer;
+  }
+`;
+const EditChannelButtonWrapper = styled.div`
+  height: 82px;
+  background: #f7f6f8;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  border-bottom-right-radius: 16px;
+
+  p {
+    background: var(--primary-color);
+    font-family: Helvetica Neue, sans-serif;
+    font-weight: bold;
+    font-size: 18px;
+    padding: 10px 20px;
+    color: #ffffff;
+    margin-right: 30px;
+    border-radius: 8px;
+    cursor: pointer;
+  }
+`;
